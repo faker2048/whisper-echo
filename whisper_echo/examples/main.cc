0e3620b21490d880c569141734b2876fe77d0f01
faker <nspyia2002@gmail.com>
2023-06-12 22:37:02 +0800

    Use whisper_context in exmpales/main.cc

--- whisper_echo/examples/main.cc
+++ whisper_echo/examples/main.cc
@@ -1,10 +1,16 @@
 #include <iostream>
 
 #include "whisper.h"
+#include "whisper_echo/whisper_context.h"
 #include "whisper_echo/whisper_params.h"
 
+using whisper::WhisperContext;
+using whisper::WhisperParams;
+
 int main(int argc, char** argv) {
-  whisper::WhisperParams params(argc, argv);
-  params.PrintUsage();
+  WhisperParams params(argc, argv);
+
+  auto context = WhisperContext::FromFile(params.model);
+
   return 0;
 }
