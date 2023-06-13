c2d35abbe9d5dee60425f4d12a4c37cbee230916
faker <nspyia2002@gmail.com>
2023-06-13 22:41:21 +0800

    WhisperApp WhisperApp::Builder impl

---
+++ whisper_echo/websocket/whisper_app.cc
@@ -0,0 +1,44 @@
+#include "whisper_echo/websocket/whisper_app.h"
+
+#include "spdlog/spdlog.h"
+#include "whisper_echo/websocket/whisper_websocket_controller.h"
+
+namespace whisper {
+
+WhisperApp::Config WhisperApp::Config::Default() {
+  return Config{
+      .host = "127.0.0.1",
+      .port = 8008,
+  };
+}
+
+WhisperApp::Builder::Builder() : config_(Config::Default()) {
+}
+
+WhisperApp::Builder& WhisperApp::Builder::SetHost(const std::string& host) {
+  config_.host = host;
+  return *this;
+}
+
+WhisperApp::Builder& WhisperApp::Builder::SetPort(int port) {
+  config_.port = port;
+  return *this;
+}
+
+WhisperApp WhisperApp::Builder::Build() {
+  return WhisperApp(std::move(config_));
+}
+
+WhisperApp::WhisperApp(Config&& config) : config_(std::move(config)) {
+}
+
+WhisperApp::WhisperApp(const Config& config) : config_(config) {
+}
+
+void WhisperApp::Run() {
+  drogon::app()
+      .setClientMaxWebSocketMessageSize(std::numeric_limits<size_t>::max())
+      .addListener(config_.host, config_.port);
+}
+
+}  // namespace whisper
