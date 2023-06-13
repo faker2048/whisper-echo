d8dca4fd1f5a9f761770899b5626d0c7a08e6e39
faker <nspyia2002@gmail.com>
2023-06-13 21:35:54 +0800

    Refactor WebSocketChat

--- whisper_echo/examples/drogon_websocket_server/web_socket_chat.cc
+++ whisper_echo/examples/drogon_websocket_server/web_socket_chat.cc
@@ -1,55 +1,59 @@
 #include <drogon/HttpAppFramework.h>
 #include <drogon/PubSubService.h>
 #include <drogon/WebSocketController.h>
+
 using namespace drogon;
 
 class WebSocketChat : public drogon::WebSocketController<WebSocketChat> {
  public:
-  virtual void handleNewMessage(const WebSocketConnectionPtr &,
-                                std::string &&,
-                                const WebSocketMessageType &) override;
-  virtual void handleConnectionClosed(const WebSocketConnectionPtr &) override;
-  virtual void handleNewConnection(const HttpRequestPtr &,
-                                   const WebSocketConnectionPtr &) override;
+  void handleNewMessage(const WebSocketConnectionPtr &,
+                        std::string &&,
+                        const WebSocketMessageType &) override;
+  void handleConnectionClosed(const WebSocketConnectionPtr &) override;
+  void handleNewConnection(const HttpRequestPtr &,
+                           const WebSocketConnectionPtr &) override;
+
   WS_PATH_LIST_BEGIN
   WS_PATH_ADD("/chat", Get);
   WS_PATH_LIST_END
+
  private:
-  PubSubService<std::string> chatRooms_;
+  PubSubService<std::string> chat_rooms_;
 };
 
 struct Subscriber {
-  std::string chatRoomName_;
+  std::string chat_room_name_;
   drogon::SubscriberID id_;
 };
-void WebSocketChat::handleNewMessage(const WebSocketConnectionPtr &wsConnPtr,
+
+void WebSocketChat::handleNewMessage(const WebSocketConnectionPtr &ws_conn_ptr,
                                      std::string &&message,
                                      const WebSocketMessageType &type) {
-  // write your application logic here
-  LOG_DEBUG << "new websocket message:" << message;
+  // Write your application logic here
+  LOG_DEBUG << "New WebSocket message: " << message;
   if (type == WebSocketMessageType::Ping) {
-    LOG_DEBUG << "recv a ping";
+    LOG_DEBUG << "Received a ping";
   } else if (type == WebSocketMessageType::Text) {
-    auto &s = wsConnPtr->getContextRef<Subscriber>();
-    chatRooms_.publish(s.chatRoomName_, message);
+    auto &s = ws_conn_ptr->getContextRef<Subscriber>();
+    chat_rooms_.publish(s.chat_room_name_, message);
   }
 }
 
 void WebSocketChat::handleConnectionClosed(const WebSocketConnectionPtr &conn) {
-  LOG_DEBUG << "websocket closed!";
+  LOG_DEBUG << "WebSocket closed!";
   auto &s = conn->getContextRef<Subscriber>();
-  chatRooms_.unsubscribe(s.chatRoomName_, s.id_);
+  chat_rooms_.unsubscribe(s.chat_room_name_, s.id_);
 }
 
 void WebSocketChat::handleNewConnection(const HttpRequestPtr &req,
                                         const WebSocketConnectionPtr &conn) {
-  LOG_DEBUG << "new websocket connection!";
+  LOG_DEBUG << "New WebSocket connection!";
   conn->send("haha!!!");
   Subscriber s;
-  s.chatRoomName_ = req->getParameter("room_name");
-  s.id_           = chatRooms_.subscribe(
-      s.chatRoomName_, [conn](const std::string &topic, const std::string &message) {
-        // Supress unused variable warning
+  s.chat_room_name_ = req->getParameter("room_name");
+  s.id_             = chat_rooms_.subscribe(
+      s.chat_room_name_, [conn](const std::string &topic, const std::string &message) {
+        // Suppress unused variable warning
         (void)topic;
         conn->send(message);
       });
