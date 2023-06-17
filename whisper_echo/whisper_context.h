ce4c173bc6ead1dadb886e0a547199f0d3f5b18b
faker <nspyia2002@gmail.com>
2023-06-18 00:53:46 +0800

    Refactor WhisperContext initialization

--- whisper_echo/whisper_context.h
+++ whisper_echo/whisper_context.h
@@ -4,14 +4,17 @@
 #include <vector>
 
 #include "whisper.h"
+#include "whisper_echo/utlis/singleton.h"
 
 namespace whisper {
 
 class WhisperContext {
  public:
-  static WhisperContext FromFile(const std::string& model_path);
-
   explicit WhisperContext(struct whisper_context* ctx);
+  WhisperContext(const std::string& model_path);
+
+  WhisperContext(const WhisperContext&)            = delete;
+  WhisperContext& operator=(const WhisperContext&) = delete;
 
   ~WhisperContext();
 
@@ -22,7 +25,9 @@ class WhisperContext {
   std::string_view GetSegmentText(int segment_index) const;
 
  private:
-  struct whisper_context* ctx;
+  struct whisper_context* ctx = nullptr;
 };
+
+using WhisperContextSingleton = Singleton<WhisperContext>;
 
 }  // namespace whisper
